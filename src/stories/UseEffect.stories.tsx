8f7b0d80647c1d1a84316393b44db8a712c5d8be
olychkam <olkaaamartynova@gmail.com>
2021-03-18 17:02:15 +0300

    useEffect /setInterval

--- src/stories/UseEffect.stories.tsx
+++ src/stories/UseEffect.stories.tsx
@@ -7,15 +7,35 @@ export default {
 export const UseState = () => {
     console.log('UseState')
     const [counter, setCounter] = useState(1);
-    const [fake, setFake]=useState(1)
+    const [fake, setFake] = useState(1)
     useEffect(() => {
         console.log('UseEffect first render')
-        document.title=counter.toString()
+        document.title = counter.toString()
     },)
     useEffect(() => {
         console.log('UseEffect counter')
-        document.title=counter.toString()
-    },[counter])
+        document.title = counter.toString()
+    }, [counter])
+
+    return <>
+        Hello, {counter} {fake}
+        <button onClick={() => {
+            setCounter(counter + 1)
+        }}>+
+        </button>
+    </>
+}
+
+export const UseSetTimeout = () => {
+    console.log('SetTimeout')
+    const [counter, setCounter] = useState(1);
+    const [fake, setFake] = useState(1)
+    useEffect(() => {
+        console.log('UseEffect first render')
+        setInterval(() => {
+            setCounter((state) => state + 1)
+        }, 1000)
+    }, [])
 
     return <>
         Hello, {counter} {fake}
