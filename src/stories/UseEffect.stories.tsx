2bde9346994ed291d01419fffed435a6534523da
olychkam <olkaaamartynova@gmail.com>
2021-03-18 14:52:05 +0300

    useEffect

---
+++ src/stories/UseEffect.stories.tsx
@@ -0,0 +1,27 @@
+import React, {useEffect, useMemo, useState} from "react";
+
+export default {
+    title: 'UseEffect'
+}
+
+export const UseState = () => {
+    console.log('UseState')
+    const [counter, setCounter] = useState(1);
+    const [fake, setFake]=useState(1)
+    useEffect(() => {
+        console.log('UseEffect first render')
+        document.title=counter.toString()
+    },)
+    useEffect(() => {
+        console.log('UseEffect counter')
+        document.title=counter.toString()
+    },[counter])
+
+    return <>
+        Hello, {counter} {fake}
+        <button onClick={() => {
+            setCounter(counter + 1)
+        }}>+
+        </button>
+    </>
+}
